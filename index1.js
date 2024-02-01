6707a559a3e077ce189c07fa03b029b4d6887d66
web1004 <gangsugi@gmail.com>
2024-02-01 16:33:12 +0900

    Modify-24.2.1

--- index1.js
+++ index1.js
@@ -5,19 +5,19 @@ $(document).ready(function(){
 
   /*브라우저 창 사이즈 변경시___________ */
   $(window).resize(function(){
-    wh = $(window).height();
-    $("html,body").stop().animate({ scrollTop:wh*a },700); 
+    location.reload();  //새로고침
+    let wh = $(window).height();
+    $("html,body").stop().animate({ scrollTop:wh*a },500);
   });
-
 
   /* 탑메뉴 클릭시______________________ */
   $("#gnb li,#dot span").click(function(){
     let num=$(this).index();
-    $("html,body").stop().animate({ scrollTop:wh*num },700); 
+    $("#tt").text(num); 
     $("#dot span").eq(num).addClass("active");
 		$("#dot span").eq(num).siblings().removeClass("active");
+    $("html,body").stop().animate({ scrollTop:wh*num },500); 
   });
-  
 
   /* 마우스휠__________________________ */
   let a = 0;  //페이지번호
@@ -25,7 +25,6 @@ $(document).ready(function(){
 	let wheel = true;
 
   $(".area").on("mousewheel DOMMouseScroll",function(e,delta) {
-
     e.preventDefault();  //요소의 기본기능해제
     if ( wheel ) {
       let n = $(this).index();
@@ -42,41 +41,41 @@ $(document).ready(function(){
       $("#tt").text(a); 
       $("#dot span").eq(a).addClass("active");
 			$("#dot span").eq(a).siblings().removeClass("active");
-      $("html,body").stop().animate({ scrollTop:wh*a },700); 	
+      $("html,body").stop().animate({ scrollTop:wh*a },500); 
 
     }
-
   });
 
-
-  /* 스크롤이벤트______________________ */
+  /*스크롤이벤트______________________ */
   $(window).scroll(function(){ 
-  
     let sc = $(document).scrollTop();
 
-    if((sc >=0) && (sc < wh)) {   //한영역 높이가 wh임
+    //한영역 높이가 wh임 
+    if((sc>=0) && (sc<wh)){  
       a=0; 
       $("#tt").text(a);
-    }
+    };
 
-    if((sc >= wh) && (sc < wh*2)) {
+    if((sc>=wh) && (sc< wh*2)){  
       a=1; 
+      $("#tt").text(a);
+
+      //콜백함수를 사용해서 첫번째 요소의 애니메이션이 완료한 후에 2번째 요소가 나오게 함
       $(".about_contents p:nth-child(2)").delay(200).animate({top:"0"},700,function(){
         $(".about_contents p:nth-child(1)").delay(200).animate({right:"250px",opacity:"1"},700);
       });
-    }
+      
+    };
 
-    if((sc >= wh*2) && (sc < wh*3)) {   
+    if((sc>=wh*2) && (sc<wh*3)){  
       a=2; 
       $("#tt").text(a);
+    };
 
-    }
-
-    if(sc >= wh*3) {   
-			a=3; 
+    if(sc>=wh*3){  
+      a=3; 
       $("#tt").text(a);
-		}    
-
+    };
   });
 
 });
